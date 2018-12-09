8eb772eb60f3f6e5324958fe5c0ba0401c25a836
Nicolas Williams <ndwill@umich.edu>
2018-12-08 23:24:48 -0500

    Beginning ubuntu setup

---
+++ setups/ubuntu.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+# For getting vim coloring
+sudo apt-get install dconf-cli
---
+++ true-installations/ubuntu/install_environment_ubuntu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# script to install development environment on a fresh install of ubuntu
+
+# TODO: Find a way to give temporary sudo priveleges to the script, once run
+
+# update repositories
+sudo apt-get update
+
+# Vim
+sudo apt-get install vim
+
+# gconf2
+sudo apt install gconf2
+
