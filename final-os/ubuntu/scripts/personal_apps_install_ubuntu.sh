bed8a19122d7ffa9ce3c864ec8778ab8a635514e
Nicolas Williams <ndwill@umich.edu>
2019-04-16 14:38:55 -0400

    fixed gimp install script

--- final-os/ubuntu/scripts/personal_apps_install_ubuntu.sh
+++ final-os/ubuntu/scripts/personal_apps_install_ubuntu.sh
@@ -2,5 +2,11 @@
 
 # Install apps for personal use on an Ubuntu development environment
 
+# gimp
+sudo apt autoremove gimp gimp-plugin-registry
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+sudo apt update
 sudo apt install gimp
+
+# discord
 sudo snap install discord
