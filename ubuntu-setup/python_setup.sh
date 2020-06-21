29b2d718103c3ba3746ee822a5e0837587f879e4
Nicolas Williams <ndwill@umich.edu>
2020-06-21 12:24:00 -0700

    Add setup of python and pip symlinks, and venv

--- ubuntu-setup/python_setup.sh
+++ ubuntu-setup/python_setup.sh
@@ -4,7 +4,10 @@
 # install python3 & pip3 if not already installed
 sudo apt install python3
 sudo apt install python3-pip
+sudo apt install python3-venv
 
+sudo ln -s /usr/bin/pip3 /usr/bin/pip
+sudo ln -s /usr/bin/python3 /usr/bin/python
 # TODO: Show dialogue to optionally symlink 'python' and 'pip' to python3 and pip3, respectively
 
 declare -a packages=(
