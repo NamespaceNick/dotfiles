f052cbd81f9c15c0549e05507e5c951448065df3
Nicolas Williams <ndwill@umich.edu>
2020-08-12 18:15:58 +0000

    Remove portfolio repo clone from setup

--- ubuntu-setup/personal_repositories.sh
+++ ubuntu-setup/personal_repositories.sh
@@ -4,8 +4,5 @@
 
 git clone git@github.com:NicolasDWilliams/test-files.git $HOME/developer/test-files
 echo "Test files repository has been cloned!"
-git clone git@github.com:NicolasDWilliams/NicolasDWilliams.github.io.git $HOME/developer/portfolio
-echo "Online portfolio repository has been cloned!"
-	
 
 echo "All personal repositories have been cloned!"
