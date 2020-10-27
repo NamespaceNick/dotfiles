740989d729c316317c9f60d29bf0018ebafa7795
Nicolas Williams <ndwill@umich.edu>
2020-10-27 14:27:34 -0700

    Add Jekyll installation

---
+++ mac-setup/utilities_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# TODO: Ensure dev tools are installed
+
+# Ruby
+# TODO: Ensure ruby version 2.5.0+
+
+
+################################### JEKYLL ###################################
+gem install jekyll bundler
