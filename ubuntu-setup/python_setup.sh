4ed6635962071deea505737e6020c07f57a5aedc
Nicolas Williams <ndwill@umich.edu>
2020-11-14 03:37:01 +0000

    Check for Python3 before adding symlink

--- ubuntu-setup/python_setup.sh
+++ ubuntu-setup/python_setup.sh
@@ -6,6 +6,7 @@ sudo apt install -y python3
 sudo apt install -y python3-pip
 sudo apt install -y python3-venv
 
-sudo ln -s /usr/bin/pip3 /usr/bin/pip
-sudo ln -s /usr/bin/python3 /usr/bin/python
+# TODO: Maybe don't have to add symlink if it's already python3?
+sudo ln -sf /usr/bin/pip3 /usr/bin/pip
+sudo ln -sf /usr/bin/python3 /usr/bin/python
 # TODO: Show dialogue to optionally symlink 'python' and 'pip' to python3 and pip3, respectively
